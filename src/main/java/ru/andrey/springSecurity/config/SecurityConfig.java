01f31220297c3ef3cac56868a800d16ec083450a
qqAndrey <63346175+botmadness111@users.noreply.github.com>
2024-02-14 16:47:57 +0300

    Spring Security Light Version
    Как было раньше:
    SecurityConfig -> AuthProviderImpl -> PersonDetailsService -> PersonDetails -> Person -> Hibernate + db.
    '->' - использует.
    
    class AuthProviderImpl имел следующую логику:
    1)Получить объект из бд
    2)сравнить имя пользователя
    3)сравнить пароль
    4)вернуть оюъект Authentication с Principal'ом внутри (данные пользователя)
    
    Тe, чтобы конфигурационный класс Spring Security работал верно, нам приходилось реализовать много других классов, которые реализует интрефейсы Spring Security.
    Но в этом и дело, что у SpringSecurity уже есть готовые интерфейсы, описывающие работу SpringSecurity.
    Поэтому, если мы хотим реализовать стандартную регистрацию пользователя, то можем логику класса AuthProviderImpl доверить SpringSecurity.
    
    Получим следующее:
    
    SecurityConfig -> PersonDetailsService -> PersonDetails -> Person -> Hibernate + db.
    
    То есть мы не будем реализовывать класс AuthProviderImpl.
    
    в SecurityConfig
    1)Внедрим PersonDetailsService
    2)Вместо внедрения SpringSecurityAuthenficationProvider, мы передадим ему наш UserDetailService.
    
    @Bean
        public PasswordEncoder getPassword() {
            return NoOpPasswordEncoder.getInstance();
        }
    
    Это шифрование пароля. Обязательно создать этот Bean!

--- src/main/java/ru/andrey/springSecurity/config/SecurityConfig.java
+++ src/main/java/ru/andrey/springSecurity/config/SecurityConfig.java
@@ -1,23 +1,34 @@
 package ru.andrey.springSecurity.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import ru.andrey.springSecurity.security.AuthProviderImpl;
+import ru.andrey.springSecurity.services.PersonDetailsService;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfiguration {
 
-    private final AuthProviderImpl authProvider;
+    //    private final AuthProviderImpl authProvider;
+    private final PersonDetailsService personDetailsService;
 
     @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
     }
 
     //Настраивает аутенфикацию
-    protected void configure(AuthenticationManagerBuilder auth){
-        auth.authenticationProvider(authProvider); //TODO
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    //    auth.authenticationProvider(authProvider);
+        auth.userDetailsService(personDetailsService); //TODO
+    }
+
+    @Bean
+    public PasswordEncoder getPassword() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
